358d9d35285cadcb9e048d8baa8f5a31f4fc19ca
likitha115 <119764047+likitha115@users.noreply.github.com>
2022-12-09 20:13:29 -0600

    Update Bank_Loan_Prediction.py

--- Bank_Loan_Prediction.py
+++ Bank_Loan_Prediction.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import pickle
-model = pickle.load('ML_Model1')
+model = pickle.load(open('C:/Users/likit/OneDrive/Documents/loan/ML_Model1.pickle','rb'))
 
 def run():
     img1 = Image.open('bank.png')
